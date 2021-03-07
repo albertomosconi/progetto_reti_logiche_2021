d182cddabf0819777529c12f0edc1699324bb5a9
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2021-03-07 11:27:14 +0100

    prevent creation of inferred latches

--- 10620673_10653349.vhd
+++ 10620673_10653349.vhd
@@ -32,7 +32,6 @@ architecture rtl of project_reti_logiche is
         SHIFT_COUNTER_STATE
     );
     signal current_state    : state_type := RESET_STATE;
-    signal next_state       : state_type := RESET_STATE;
     -- Maximum pixel value in the original image
     signal max_pixel_value  : std_logic_vector(7 downto 0) := x"00";
     -- Minimum pixel value in the original image
@@ -50,135 +49,223 @@ architecture rtl of project_reti_logiche is
 
 begin
 
-process (i_clk, i_rst)
-begin
-    if i_rst = '1' then
-        current_state <= RESET_STATE;
-        
-    elsif rising_edge(i_clk) then
-        current_state <= next_state;
-        
-    end if;
-end process;
-
-process (current_state, i_start, i_data, pos_count) is
-    -- Count the minimum number of bits to 
-    --variable pos_count      : integer := 7;
+process (i_clk, i_rst) is
     -- Store the delta_value incremented by 1
     variable delta_plus_one : std_logic_vector(7 downto 0);
 
 begin
-    case current_state is
-        -- RESET STATE: waiting for start signal
-        when RESET_STATE =>
+    if i_rst = '1' then
+        -- Set values to default
+        o_address <= (others => '0');
+        o_done <= '0';
+        o_en <= '0';
+        o_we <= '0';
+        o_data <= (others => '0');
+        current_address <= (others => '0');
+        n_col <= (others => '0');
+        n_rig <= (others => '0');
+        pos_count <= 7;
+        max_pixel_value <= (others => '0');
+        min_pixel_value <= (others => '1');
+        delta_value <= (others => '0');
+        temp_pixel <= (others => '0');
+        new_pixel_value <= (others => '0');
+        shift_level <= (others => '0');
         
-            -- Set values to default
-            o_address <= (others => '0');
-            o_done <= '0';
-            o_en <= '0';
-            o_we <= '0';
-            o_data <= (others => '0');
-            current_address <= (others => '0');
+        current_state <= RESET_STATE;
         
-            -- If the start signal is received, begin computation, otherwise do nothing
-            if i_start = '1' then
-                -- Signal to the RAM that we want to read
+    elsif rising_edge(i_clk) then
+    
+        case current_state is
+            -- RESET STATE: waiting for start signal
+            when RESET_STATE =>
+            
+                -- Set values to default
+                o_address <= (others => '0');
+                o_done <= '0';
+                o_en <= '0';
+                o_we <= '0';
+                o_data <= (others => '0');
+                current_address <= (others => '0');
+                n_col <= (others => '0');
+                n_rig <= (others => '0');
+                pos_count <= 7;
+                max_pixel_value <= (others => '0');
+                min_pixel_value <= (others => '1');
+                delta_value <= (others => '0');
+                temp_pixel <= (others => '0');
+                new_pixel_value <= (others => '0');
+                shift_level <= (others => '0');
+                current_state <= RESET_STATE;
+                
+                -- If the start signal is received, begin computation, otherwise do nothing
+                if i_start = '1' then
+                    -- Signal to the RAM that we want to read
+                    o_en <= '1';
+                    current_state <= WAIT_READ_STATE;
+                end if;
+            
+            -- WAIT READ STATE: waiting for the RAM to give the requested memory contents
+            when WAIT_READ_STATE =>
+                
+                o_address <= current_address;
+                o_done <= '0';
                 o_en <= '1';
-                next_state <= WAIT_READ_STATE;
-            end if;
-        
-        -- WAIT READ STATE: waiting for the RAM to give the requested memory contents
-        when WAIT_READ_STATE =>
-        
-            -- Skip a cycle before reading in memory
-            next_state <= READ_STATE;
-        
-        -- READ STATE: reading and saving contents from the RAM
-        when READ_STATE =>
-        
-            -- Memory with address 0 contains the number of columns
-            if current_address = x"0000" then
-                -- Save the number of columns
-                n_col <= i_data;
-                
-                -- Move on to the next address to read
-                current_address <= current_address + 1;
-                o_address <= current_address + 1;
-                
-                next_state <= WAIT_READ_STATE;
+                o_we <= '0';
+                o_data <= (others => '0');
+                n_col <= n_col;
+                n_rig <= n_rig;
+                current_address <= current_address;
+                pos_count <= 7;
+                max_pixel_value <= max_pixel_value;
+                min_pixel_value <= min_pixel_value;
+                delta_value <= delta_value;
+                temp_pixel <= temp_pixel;
+                new_pixel_value <= new_pixel_value;
+                shift_level <= shift_level;
+            
+                -- Skip a cycle before reading in memory
+                current_state <= READ_STATE;
+            
+            -- READ STATE: reading and saving contents from the RAM
+            when READ_STATE =>
+                -- Set values to default
+                o_address <= current_address;
+                o_done <= '0';
+                o_en <= '1';
+                o_we <= '0';
+                o_data <= (others => '0');
+                n_col <= n_col;
+                n_rig <= n_rig;
+                current_address <= current_address;
+                pos_count <= 7;
+                max_pixel_value <= max_pixel_value;
+                min_pixel_value <= min_pixel_value;
+                delta_value <= delta_value;
+                temp_pixel <= temp_pixel;
+                new_pixel_value <= new_pixel_value;
+                shift_level <= shift_level;
+            
+                -- Memory with address 0 contains the number of columns
+                if current_address = x"0000" then
+                    -- Save the number of columns
+                    n_col <= i_data;
                     
-            -- Memory with address 1 contains the number of rows 
-            elsif current_address = x"0001" then
-                -- Save the number of rows
-                n_rig <= i_data;
-                
-                -- Move on to the next address to read
-                current_address <= current_address + 1;
-                o_address <= current_address + 1;
-                
-                next_state <= WAIT_READ_STATE;
-                
-            -- Next addresses contain the pixel values of the original image
-            elsif current_address < n_col * n_rig + 2 then
-                -- If a new maximum value is found, save it
-                if i_data > max_pixel_value then
-                    max_pixel_value <= i_data;
-                end if;
-                
-                -- If a new minimum value if found, save it
-                if i_data < min_pixel_value then
-                    min_pixel_value <= i_data;
-                end if;
-                
-                -- 0 and 255 are respectively the smallest and biggest values possible.
-                -- If we have already encountered them, there's no need to keep going
-                -- and the equalization process can start
-                --if min_pixel_value = x"00" and max_pixel_value = x"ff" then
-                --    current_address <= x"0001";
-                --end if;
-                
-                -- Check if we've reached the last pixel or not
-                if current_address < n_col * n_rig + 1 then
-                    -- If we're not at the end, move on to the next address to read
+                    -- Move on to the next address to read
                     current_address <= current_address + 1;
                     o_address <= current_address + 1;
                     
-                    next_state <= WAIT_READ_STATE;
+                    current_state <= WAIT_READ_STATE;
+                        
+                -- Memory with address 1 contains the number of rows 
+                elsif current_address = x"0001" then
+                    -- Save the number of rows
+                    n_rig <= i_data;
                     
-                else
-                    -- If we've reached the end, calculate delta_value
-                    delta_value <= max_pixel_value - min_pixel_value;
-                    -- Change state to calculate the shift level
-                    next_state <= SHIFT_COUNTER_STATE;
+                    -- Move on to the next address to read
+                    current_address <= current_address + 1;
+                    o_address <= current_address + 1;
+                    
+                    current_state <= WAIT_READ_STATE;
+                    
+                -- Next addresses contain the pixel values of the original image
+                elsif current_address < n_col * n_rig + 2 then
+                    -- If a new maximum value is found, save it
+                    if i_data > max_pixel_value then
+                        max_pixel_value <= i_data;
+                    end if;
+                    
+                    -- If a new minimum value if found, save it
+                    if i_data < min_pixel_value then
+                        min_pixel_value <= i_data;
+                    end if;
+                    
+                    -- 0 and 255 are respectively the smallest and biggest values possible.
+                    -- If we have already encountered them, there's no need to keep going
+                    -- and the equalization process can start
+                    --if min_pixel_value = x"00" and max_pixel_value = x"ff" then
+                    --    current_address <= x"0001";
+                    --end if;
+                    
+                    -- Check if we've reached the last pixel or not
+                    if current_address < n_col * n_rig + 1 then
+                        -- If we're not at the end, move on to the next address to read
+                        current_address <= current_address + 1;
+                        o_address <= current_address + 1;
+                        
+                        current_state <= WAIT_READ_STATE;
+                        
+                    else
+                        -- If we've reached the end, calculate delta_value
+                        delta_value <= max_pixel_value - min_pixel_value;
+                        -- Change state to calculate the shift level
+                        current_state <= SHIFT_COUNTER_STATE;
+                        
+                    end if;
+                    
+                end if;
+            
+            -- SHIFT COUNTER STATE: subtract number of leading zeros from the total 
+            -- number of bits in the vector delta_value + 1
+            when SHIFT_COUNTER_STATE =>
+                
+                -- Set values to default
+                o_address <= current_address;
+                o_done <= '0';
+                o_en <= '1';
+                o_we <= '0';
+                o_data <= (others => '0');
+                n_col <= n_col;
+                n_rig <= n_rig;
+                current_address <= current_address;
+                pos_count <= 7;
+                max_pixel_value <= max_pixel_value;
+                min_pixel_value <= min_pixel_value;
+                delta_value <= delta_value;
+                temp_pixel <= temp_pixel;
+                new_pixel_value <= new_pixel_value;
+                shift_level <= shift_level;
+                
+                delta_plus_one := max_pixel_value - min_pixel_value + 1;
+                
+                if delta_plus_one(pos_count) = '0' then
+                    pos_count <= pos_count - 1;
+                    current_state <= SHIFT_COUNTER_STATE;
+                    
+                elsif delta_plus_one(pos_count) = '1' then
+                    shift_level <= std_logic_vector(TO_UNSIGNED(8 - pos_count, 4));
+                    
+                    -- Move back to the first pixel of the image
+                    current_address <= x"0002";
+                    o_address <= x"0002";
+                    
+                    -- Start reading the original bytes again to recalculate values
+                    current_state <= WAIT_READ_STATE;
                     
                 end if;
                 
-            end if;
+            -- Undefined state: do nothing
+            when others =>
+                -- Set values to default
+                o_address <= current_address;
+                o_done <= '0';
+                o_en <= '1';
+                o_we <= '0';
+                o_data <= (others => '0');
+                n_col <= n_col;
+                n_rig <= n_rig;
+                current_address <= current_address;
+                pos_count <= 7;
+                max_pixel_value <= max_pixel_value;
+                min_pixel_value <= min_pixel_value;
+                delta_value <= delta_value;
+                temp_pixel <= temp_pixel;
+                new_pixel_value <= new_pixel_value;
+                shift_level <= shift_level;             
+                current_state <= current_state;
         
-        -- SHIFT COUNTER STATE: subtract number of leading zeros from the total 
-        -- number of bits in the vector delta_value + 1
-        when SHIFT_COUNTER_STATE =>
-            delta_plus_one := max_pixel_value - min_pixel_value + 1;
-            
-            if delta_plus_one(pos_count) = '0' then
-                pos_count <= pos_count - 1;
-                
-            elsif delta_plus_one(pos_count) = '1' then
-                shift_level <= std_logic_vector(TO_UNSIGNED(8 - pos_count, 4));
-                
-                -- Move back to the first pixel of the image
-                current_address <= x"0002";
-                o_address <= x"0002";
-                
-                -- Start reading the original bytes again to recalculate values
-                next_state <= WAIT_READ_STATE;
-                
-            end if;
-            
-        -- Undefined state: do nothing
-        when others =>        
-    
-    end case;
+        end case;
+    end if;
         
 end process;
 
